899bb576f1bf0f31f5204e6aea67d825f5c98807
GastonSKL <gastonlskamlec@gmail.com>
2021-08-18 00:50:57 -0300

    Creación de Navbar básica

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        titulo: 'Inicio',
+        link: '#',
+        cName: 'nav-links' 
+    },
+
+    {   
+        titulo: 'Servicios',
+        link: '#',
+        cName: 'nav-links' 
+    },
+    {
+        titulo: 'Productos',
+        link: '#',
+        cName: 'nav-links' 
+    },
+    {
+        titulo: 'Contactanos',
+        link: '#',
+        cName: 'nav-links' 
+    },
+    {
+        titulo: 'Logueate',
+        link: '#',
+        cName: 'nav-links' 
+    }
+]
